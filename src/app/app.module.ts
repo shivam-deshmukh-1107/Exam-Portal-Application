6b40621f3aa0b4c98269548eea4b7e7eeb3abcbd
Shivam <shivamsdeshmukh1107@gmail.com>
2023-09-09 14:41:49 +0530

    size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { LoadQuizComponent } from './pages/user/load-quiz/load-quiz.component';
 import { InstructionsComponent } from './pages/user/instructions/instructions.component';
 import { StartComponent } from './pages/user/start/start.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
 
 
 @NgModule({
@@ -82,6 +83,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatSlideToggleModule,
     MatSelectModule,
     MatProgressSpinnerModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule.forRoot({ showForeground: true }),
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent],
